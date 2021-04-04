a5a7c28b7a741bdfc6ca365c8ecb32c4d143f818
Win Eiwwongcharoen <win.eiwwongcharoen@gmail.com>
2021-04-04 20:18:58 +0700

    test react-native-rename

--- android/app/src/main/java/com/rennerwin/rnconfigdemo/MainActivity.java
+++ android/app/src/main/java/com/rennerwin/rnconfigdemo/MainActivity.java
@@ -1,4 +1,4 @@
-package com.configdemo;
+package com.rennerwin.rnconfigdemo;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "ConfigDemo";
+    return "RNConfigDemo";
   }
 }
